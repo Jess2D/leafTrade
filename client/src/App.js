fdbad085e3ff392c8cd0ef0eb264735c2af8b078
Jessica Silveira <jessicap_silveira@hotmail.com>
2022-09-07 08:13:08 +1200

    feat: Navbar Sprint 3.1

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import { Route, Routes } from "react-router-dom";
 
 // We import all the components we need in our app
 import Home from "./components/home/Home";
-import Navbar from "./components/navbar/Navbar";
+import TopBar from "./components/navbar/Topbar";
 import Product from "./components/product/ProductView";
 import NewListing from "./components/listing/NewListing";
 import MagageListing from "./components/listing/ManageListing";
@@ -15,7 +15,7 @@ import Footer from "./components/footer/Footer";
 const App = () => {
   return (
     <div>
-      <Navbar />
+      <TopBar />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/product/:id" element={<Product />} />
---
+++ client/src/components/navbar/Topbar.js
@@ -0,0 +1,62 @@
+import React from "react";
+import styled from "styled-components";
+import { Button, Container, Navbar, Nav, Form } from "react-bootstrap";
+import logo from "../../assets/logo.png";
+
+const Background = styled.div`
+  background: #506053;
+  color: #fffffd;
+`;
+const Padding = styled.div`
+  padding: 5px;
+`;
+
+/**
+ * @description Represents a Navbar view Component for the whole app
+ */
+
+const TopBar = () => {
+  return (
+    <div>
+      <Background>
+        <Navbar expand="lg">
+          <Container fluid>
+            <Navbar.Brand href="#">
+              {" "}
+              <img src={logo} alt="Logo" width={"89px"} />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="me-auto"
+                style={{ maxHeight: "100px" }}
+                navbarScroll
+              >
+                <Nav.Link className="text-white" href="#action1">
+                  {" "}
+                  Indoor Plants{" "}
+                </Nav.Link>
+                <Nav.Link className="text-white" href="#action2">
+                  {" "}
+                  Outdoor Plants{" "}
+                </Nav.Link>
+                <Nav.Link className="text-white" href="#action3">
+                  {" "}
+                  Kitchen Herbs{" "}
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+            <Padding>
+              <Button variant="outline-light">Sell</Button>
+            </Padding>
+            <Padding>
+              <Button variant="outline-light">Shop</Button>
+            </Padding>
+          </Container>
+        </Navbar>
+      </Background>
+    </div>
+  );
+};
+
+export default TopBar;
