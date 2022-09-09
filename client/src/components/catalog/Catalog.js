58c981030d7555d5a8f9912341b663d54981743d
Jessica Silveira <jessicap_silveira@hotmail.com>
2022-09-09 16:46:02 +1200

    feat: add img catalog'

---
+++ client/src/components/catalog/Catalog.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+/**
+ * @description Represents a Login view Component for the whole app
+ */
+
+const Catalog = () => {
+  return (
+    <div>
+      <Container>Catalog</Container>
+    </div>
+  );
+};
+
+export default Catalog;
