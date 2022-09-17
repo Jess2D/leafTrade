fbaf2d15565003587f3ae6853b03210359c31bb9
Jessica Silveira <jessicap_silveira@hotmail.com>
2022-09-18 10:52:49 +1200

    User component

--- client/src/components/review/Reviews.js
+++ client/src/components/review/Reviews.js
@@ -1,7 +1,8 @@
 import React from "react";
-import Product from "../product/Product";
 import { useParams } from "react-router";
 import styled from "styled-components";
+import Product from "../product/Product";
+import User from "../user/User";
 
 const StarsOuter = styled.div`
   padding: 32px;
@@ -16,11 +17,15 @@ export default function Reviews() {
   const params = useParams();
   const id = params.id.toString();
   const product = Product(id);
+  const userID = product.userID.user_id;
+  console.log(userID);
+  const user = User(userID);
   console.log(product.userID.user_id);
+  console.log(user.name);
   return (
     <div>
       {product.name}
-      {product.reviews[0].rate}
+      {user.name}
     </div>
   );
 }
---
+++ client/src/components/user/User.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router";
+
+export default function User(user_id) {
+  const [item, setitem] = useState({
+    name: "",
+    _id: "",
+    photo: "",
+    users: [],
+  });
+  const params = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function fetchData() {
+      const id = user_id;
+      const response = await fetch(`http://localhost:5000/user/${id}`);
+
+      if (!response.ok) {
+        const message = `An error has occurred: ${response.statusText}`;
+        window.alert(message);
+        return;
+      }
+
+      const user = await response.json();
+      if (!user) {
+        window.alert(`user with id ${id} not found`);
+        navigate("/");
+        return;
+      }
+
+      setitem(user);
+    }
+
+    fetchData();
+
+    return;
+  }, [params.id, navigate]);
+
+  // This following section will display the item that takes input from the user to update the data.
+  return item;
+}
