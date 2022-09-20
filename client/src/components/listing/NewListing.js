aa607b3189ceaef11dc15ec0f442884e1937ec31
Maira <99157084+mairalds@users.noreply.github.com>
2022-09-20 19:11:39 +1200

    feat:newListing
    Completed - to review

--- client/src/components/listing/NewListing.js
+++ client/src/components/listing/NewListing.js
@@ -98,7 +98,6 @@ export default function NewListing() {
           </Container>
         </PurpleSection>
       </div>
-
       <div>
         <Padding12>
           <Padding32>
@@ -114,44 +113,43 @@ export default function NewListing() {
                     <Form.Control id="name" value={form.img} onChange={(e) => updateForm({ img: e.target.value })}
                       type="text" placeholder="Paste your URL here" />
                   </Form.Group>
-                <Form.Group className="mb-3" controlId="formCategory">
-                  <Form.Label className="fw-bold">Category</Form.Label>
-                  <Form.Select id="category" value={form.category} onChange={(e) => updateForm({ category: e.target.value })}>
-                    <option>Choose one</option>
-                    <option>Indoor Plants</option>
-                    <option>Outdoor Plants</option>
-                    <option>Kitchen Garden</option>
-                  </Form.Select>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formName">
-                  <Form.Label className="fw-bold">Title</Form.Label>
-                  <Form.Control id="name" value={form.name} onChange={(e) => updateForm({ name: e.target.value })}
-                    type="text" placeholder="Write a title that will spark interest" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formPrice">
-                  <Form.Label className="fw-bold">Price</Form.Label>
-                  <Form.Control id="price" value={form.price} onChange={(e) => updateForm({ price: e.target.value })}
-                    type="text" placeholder="How much does it cost?" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formQuantity">
-                  <Form.Label className="fw-bold">Quantity</Form.Label>
-                  <Form.Control id="quantity" value={form.quantity} onChange={(e) => updateForm({ quantity: e.target.value })}
-                    type="text" placeholder="How many do you have available?" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formDescription">
-                  <Form.Label className="fw-bold">Description</Form.Label>
-                  <Form.Control id="description" value={form.description} onChange={(e) => updateForm({ description: e.target.value })}
-                    as="textarea" rows={3} placeholder="Keep it simple, using clear, descriptive words and aiming for positive connotations" />
-                </Form.Group>
-                <div class="d-grid gap-2 col-6 mx-auto">
-                  <Button className="fw-bold" variant="dark" type="submit">
-                    PUBLISH
-                  </Button>
-                </div>
-              </Form>
-            </FormSize>
-          </div>
-        </Padding32>
+                  <Form.Group className="mb-3" controlId="formCategory">
+                    <Form.Label className="fw-bold">Category</Form.Label>
+                    <Form.Select id="category" value={form.category} onChange={(e) => updateForm({ category: e.target.value })}>
+                      <option>Indoor Plants</option>
+                      <option>Outdoor Plants</option>
+                      <option>Kitchen Garden</option>
+                    </Form.Select>
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formName">
+                    <Form.Label className="fw-bold">Title</Form.Label>
+                    <Form.Control id="name" value={form.name} onChange={(e) => updateForm({ name: e.target.value })}
+                      type="text" placeholder="Write a title that will spark interest" />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formPrice">
+                    <Form.Label className="fw-bold">Price</Form.Label>
+                    <Form.Control id="price" value={form.price} onChange={(e) => updateForm({ price: e.target.value })}
+                      type="text" placeholder="How much does it cost?" />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formQuantity">
+                    <Form.Label className="fw-bold">Quantity</Form.Label>
+                    <Form.Control id="quantity" value={form.quantity} onChange={(e) => updateForm({ quantity: e.target.value })}
+                      type="text" placeholder="How many do you have available?" />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formDescription">
+                    <Form.Label className="fw-bold">Description</Form.Label>
+                    <Form.Control id="description" value={form.description} onChange={(e) => updateForm({ description: e.target.value })}
+                      as="textarea" rows={3} placeholder="Keep it simple, using clear, descriptive words and aiming for positive connotations" />
+                  </Form.Group>
+                  <div class="d-grid gap-2 col-6 mx-auto">
+                    <Button className="fw-bold" variant="dark" type="submit">
+                      PUBLISH
+                    </Button>
+                  </div>
+                </Form>
+              </FormSize>
+            </div>
+          </Padding32>
         </Padding12>
       </div >
     </div >
