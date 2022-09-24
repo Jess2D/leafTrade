d06e36279103b468e6f899f56df47cc01945004c
Jessica Silveira <jessicap_silveira@hotmail.com>
2022-09-25 00:33:48 +1200

    fix:fix homepage

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -4,6 +4,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 import bgheroimage from "../../assets/homeassets/bgheroimage.jpeg";
 import peacelily from "../../assets/homeassets/peacelilysqr.jpeg";
 import snakeplant from "../../assets/homeassets/snakesqr.jpeg";
@@ -28,26 +29,23 @@ const MainTitle = styled.div`
   font-weight: bold;
   font-family: "Raleway";
 `;
+const Space = styled.div`
+  padding-bottom: 32px;
+`;
 
 const PrimaryButton = styled.div`
   position: absolute;
-  width: 250px;
-  height: 64px;
-  left: calc(50% - 250px / 2);
+  left: calc(50% - 50px / 2);
   top: 826px;
-  background: #0d1321;
-  border-radius: 5px;
   font-family: "Raleway";
   text-align: center;
-  font-size: 28px;
+  font-size: 48px;
   font-weight: bold;
-  color: #fffffd;
-  line-height: 64px;
 `;
 
 const Home = () => {
   return (
-    <div>
+    <div className="">
       <Imagebgmain>
         <Container fluid>
           <MainTitle>
@@ -56,86 +54,76 @@ const Home = () => {
               <p>your life</p>
             </div>
           </MainTitle>
-          <p className="text-center">
+          <p className="text-center ">
             Your online shop for buying indoor plants, outdoor plants & kitchen
             herbs
           </p>
           <PrimaryButton>
-            <Link to="/catalog">Shop</Link>
+            <Button variant="dark" href="/catalog" type="button" size="lg">
+              Shop
+            </Button>
           </PrimaryButton>
         </Container>
       </Imagebgmain>
 
       {/* Air purifying */}
-      <Container>
-        <Row>
-          <Col>
-            <h1 className="text-center text-wrap">Air purifying plants</h1>
-            <p className="text-center">
-              Looking to reduce toxins in your home? These air cleaning plants
-              will help you to improve air quality
-            </p>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={peacelily} />
-              <Card.Body>
-                <Card.Title>Peace Lily</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Card.Link href="#">Check it out</Card.Link>
-              </Card.Body>
-            </Card>
-          </Col>
+      <Space>
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="text-center text-wrap">Air purifying plants</h1>
+              <p className="text-center">
+                Looking to reduce toxins in your home? These air cleaning plants
+                will help you to improve air quality
+              </p>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Card style={{ width: "286px" }}>
+                <Card.Img variant="top" src={peacelily} />
+                <Card.Body>
+                  <Card.Title>Peace Lily</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Card.Link href="/catalog">Check it out</Card.Link>
+                </Card.Body>
+              </Card>
+            </Col>
 
-          <Col>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={snakeplant} />
-              <Card.Body>
-                <Card.Title>Snake Plant</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Card.Link href="#">Check it out</Card.Link>
-              </Card.Body>
-            </Card>
-          </Col>
+            <Col>
+              <Card style={{ width: "286px" }}>
+                <Card.Img variant="top" src={snakeplant} />
+                <Card.Body>
+                  <Card.Title>Snake Plant</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Card.Link href="/catalog">Check it out</Card.Link>
+                </Card.Body>
+              </Card>
+            </Col>
 
-          <Col>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={aloevera} />
-              <Card.Body>
-                <Card.Title>Aloe Vera</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Card.Link href="#">Check it out</Card.Link>
-              </Card.Body>
-            </Card>
-          </Col>
+            <Col>
+              <Card style={{ width: "286px" }}>
+                <Card.Img variant="top" src={aloevera} />
+                <Card.Body>
+                  <Card.Title>Aloe Vera</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Card.Link href="/catalog">Check it out</Card.Link>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </Space>
 
-          <Col>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={rubberplant} />
-              <Card.Body>
-                <Card.Title>Rubber Plant</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Card.Link href="#">Check it out</Card.Link>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-      <div></div>
       {/* TODO: Fresh Herbs */}
 
       {/* TODO: Grow your Business */}
